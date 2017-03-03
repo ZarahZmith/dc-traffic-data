b98195e146437d81cf1c412116594d68dd821528
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-03 16:06:29 -0500

    Found the average fine amount for moving violations

--- moving.js
+++ moving.js
@@ -4,28 +4,51 @@ let parseFunction = require('./parse.js');
 function movingViolations() {
 
   let movingData = parseFunction('./traffic-data/simple_data/moving_jan_2016.csv');
-  // console.log(movingData); //prints out an array of arrays
+  // console.log('prints out an array of arrays that contain moving data', movingData);
+
 
   let frequency = {};
+  let fineTotal = 0;
+  let numberOfTickets = 0;
 
+//forEach start
   movingData.forEach(function commonViolation(row){
-    // console.log(row[17]); //prints out the content of the 17th position within the array
+    // console.log('this gives me the content of the index 17 within my array', row[17]);
 
-    // I then want to compare the array to how often it occurs so i can hold on to the type of parking violation that occurs the most
-    //
-    //
-    // general set up for frequency array via iteration hw assignment
+
+/* -------------------------------------------------------- */
+    // ticket type frequency
     if(!frequency[row[17]]) {
       frequency[row[17]] = 1;
     } else {
       frequency[row[17]] = frequency[row[17]] + 1;
     }
-  });
+/* -------------------------------------------------------- */
 
-  // console.log("this logs out the frequency", frequency);
+
+/* -------------------------------------------------------- */
+    //average fine amount
+
+    let fineAmount = Number(row[10]);
+    if (fineAmount) {
+      numberOfTickets++;
+      fineTotal = fineAmount + fineTotal;
+    }
+/* -------------------------------------------------------- */
+
+  });
+// forEach end
+
+  // console.log("this logs out the ticket type frequency", frequency);
+  // console.log('this logs out the total amount of fines collected', fineTotal);
+  // console.log( 'how many tickets are there?', numberOfTickets );
+  console.log('average fine amount', fineTotal/numberOfTickets);
+
+/* -------------------------------------------------------- */
+  /** What was the most common violation type for a moving violation?*/
 
   let specificViolation = Object.keys(frequency); //this spits out a list object properties in my array
-  // console.log(specificViolation);
+  // console.log('this logs out the most frequent violation', specificViolation);
   let highestViolation = {
     name: 'default',
     count: 0
@@ -36,10 +59,11 @@ function movingViolations() {
       highestViolation.name = violationCode;
     }
   });
-  console.log(highestViolation);
+  console.log('logs out the name and count of the highest violation',highestViolation);
+/* -------------------------------------------------------- */
 
 
-
+  // return some collection of the responses
 }
 
 movingViolations();
