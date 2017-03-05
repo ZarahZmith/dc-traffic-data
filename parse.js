6f62e74c9f6232c6afede0e90dc18e32804d11a4
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-05 12:31:58 -0500

    Slight language change to file, no content changes

--- parse.js
+++ parse.js
@@ -3,9 +3,9 @@
 module.exports = function parseFunction(fileName) {
 
   let fileSystem = require('fs');
-  let movingTrafficData = fileSystem.readFileSync(fileName);
+  let selectTrafficData = fileSystem.readFileSync(fileName);
 
-  let stringValue = movingTrafficData.toString();
+  let stringValue = selectTrafficData.toString();
   let rows = stringValue.split('\n');
 
   let data = [];
