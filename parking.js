96a10296e8f0462c0232c173a64d2e221e6571b3
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-05 19:56:42 -0500

    Found most common state to recieve a parking ticket

--- parking.js
+++ parking.js
@@ -1,6 +1,9 @@
 let parseFunction = require('./parse.js');
 
+// ticket type frequency --> for most common violation type
 frequency = {};
+// for most common state licence plate
+stateFrequency = {};
 
 function parkingViolations() {
 
@@ -8,10 +11,20 @@ function parkingViolations() {
 
   parkingData.forEach(function commonViolation(row){
 /* -------------------------------------------------------- */
+    // ticket type frequency --> for most common violation type
     if(!frequency[row[9]]) {
       frequency[row[9]] = 1;
     } else {
       frequency[row[9]] = frequency[row[9]] + 1;
+    }
+/* -------------------------------------------------------- */
+
+/* -------------------------------------------------------- */
+    // for most common state licence plate
+    if(!stateFrequency[row[12]]) {
+      stateFrequency[row[12]] = 1;
+    } else {
+      stateFrequency[row[12]] = stateFrequency[row[12]] + 1;
     }
 /* -------------------------------------------------------- */
   });
@@ -29,6 +42,21 @@ function parkingViolations() {
     }
   });
   console.log('What was the most common violation type for a parking ticket?', highestViolation.name);
+  //How many different types of parking tickets were issued?
+  console.log('How many different types of parking tickets were issued?', specificViolation.length);
+  //What state license plate gets the most tickets?
+  let specificStateViolation = Object.keys(stateFrequency);
+  let stateViolation = {
+    name: 'default',
+    count: 0
+  };
+  specificStateViolation.forEach(function mostFrequentViolation(stateViolationCode){
+    if (stateFrequency[stateViolationCode] > stateViolation.count) {
+      stateViolation.count = stateFrequency[stateViolationCode];
+      stateViolation.name = stateViolationCode;
+    }
+  });
+  console.log('What state license plate gets the most tickets?', stateViolation.name);
 
 }
 
